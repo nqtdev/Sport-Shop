84a4773b1bab75dbc5ddc133ac1a179a43234d9b
Tien <nguyenquoctien2000@gmail.com>
2022-06-03 11:09:54 +0700

    update jersey

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -1,4 +1,34 @@
 // SLICK SLIDER MAIN PAGES
+// $(document).ready(function () {
+//   $(".slider-featured").slick({
+//     infinite: true,
+//     dots: false,
+//     slidesToShow: 4,
+//     slidesToScroll: 2,
+//     prevArrow:
+//       "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+//     nextArrow:
+//       "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+//     responsive: [
+//       {
+//         breakpoint: 1025,
+//         settings: {
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//           infinite: true,
+//         },
+//       },
+//       {
+//         breakpoint: 450,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//         },
+//       },
+//     ],
+//   });
+// });
+
 $(document).ready(function () {
   $(".slider-product").slick({
     infinite: true,
@@ -28,7 +58,6 @@ $(document).ready(function () {
     ],
   });
 });
-
 $(document).ready(function () {
   $(".slider-browse").slick({
     infinite: true,
